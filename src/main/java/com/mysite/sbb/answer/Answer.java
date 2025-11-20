d459ed1bbcc5e647681459f4469836d3cdc18d86
Minji-032 <5h6vmm@gmail.com>
2025-11-20 14:46:36 +0900

    2-07 질문 목록 만들기

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,5 +1,6 @@
-package com.mysite.sbb;
+package com.mysite.sbb.answer;
 
+import com.mysite.sbb.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,5 +21,5 @@ public class Answer {
     private Question question;
 
     @Column(columnDefinition = "TEXT")
-    private String answer;
+    private String content;
 }
